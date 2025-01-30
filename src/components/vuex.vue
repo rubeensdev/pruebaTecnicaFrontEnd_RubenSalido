71ffcf593203f781213fc41814530ff81351ee84
Tu Nombre <rubeensdev@gmail.com>
2025-01-30 12:36:19 +0100

    Commit prueba tecnica

---
+++ src/components/vuex.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <button @click="changeMessage">Change Message</button>
+  </div>
+</template>
+<script>
+import { mapState, mapMutations } from "vuex";
+export default {
+  computed: {
+    ...mapState(["message"]),
+  },
+  methods: {
+    ...mapMutations(["setMessage"]),
+    changeMessage() {
+      this.setMessage("hola a todos");
+    },
+  },
+};
+</script>
